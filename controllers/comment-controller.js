2a418c52bbdecefe1143a5643840e2474f5b7d87
Ragil14 <rahil.gilani@gmail.com>
2022-03-17 16:43:37 -0500

    update addComment() Method, add _id & $push Method to it

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,8 +3,24 @@ const { Comment, Pizza } = require('../models');
 // Object
 const commentController = {
   // add comment to pizza Method
-  addComment() {
-
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
   },
 
   // remove comment Method
