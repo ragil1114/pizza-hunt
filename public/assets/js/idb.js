4a5d9ed5ea670d3894f806d4a153d59cddc1b571
Ragil14 <rahil.gilani@gmail.com>
2022-03-21 19:32:32 -0500

    insert comment

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -28,7 +28,7 @@ request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection.
 function saveRecord(record) {
     // open a new Transaction with the db w/ read and write permissions 
     const transaction = db.transaction(['new_pizza'], 'readwrite');
