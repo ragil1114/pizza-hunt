829ae89e6c0a93b550c80676bb2b06692a03cf0d
Ragil14 <rahil.gilani@gmail.com>
2022-03-21 19:01:45 -0500

    insert comments

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -30,12 +30,12 @@ request.onerror = function(event) {
 
 // This function will be executed if we attempt to submit a new pizza and there's no internet connection
 function saveRecord(record) {
-    // open a new transaction with the database w/ read and write permissions 
+    // open a new Transaction with the db w/ read and write permissions 
     const transaction = db.transaction(['new_pizza'], 'readwrite');
   
-    // access the object store for `new_pizza`
+    // access the Object Store for `new_pizza`
     const pizzaObjectStore = transaction.objectStore('new_pizza');
   
-    // add record to your store with add method
+    // add data to your Store with add Method
     pizzaObjectStore.add(record);
 }
